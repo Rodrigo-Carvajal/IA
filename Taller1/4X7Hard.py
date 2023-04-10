f2cd707b6f2251f582dda6daa1257f4190242c88
Rodrigo-Carvajal <rodrigo.carvajal.ramos@gmail.com>
2023-04-10 18:46:45 -0400

    commit

--- Taller1/4X7Hard.py
+++ Taller1/4X7Hard.py
@@ -1,6 +1,5 @@
 from logicpuzzles import *
-#link: https://logic.puzzlebaron.com/play.php?u2=34df7d74038a4529bd3b62c583104ed4
-
+from time import perf_counter
 # Hay 4 marineros que compitieron en dar una vuelta al mundo en la menor cantidad de días
 marinero1 = (270, var(), var(), var())
 marinero2 = (274, var(), var(), var())
@@ -19,16 +18,17 @@ marineros = (marinero1, marinero2, marinero3, marinero4, marinero5, marinero6, m
 
 #marinero = (dias, nombre marinero, nombre bote, tipo de bote)
 def carrera(marineros):
-  return lall(    
+  return lall(
+    #2. The person who finished in 282 days sailed the Confluence.
+    eq((282, var(), 'Confluence', var()), marinero4),
     #3. The competitor in the sloop took 4 fewer days to finish than Neal Bundt.
-    left_of(marineros, (var(), var(), var(), 'Sloop'), ('Neal Bundt', var(), var(), var())),
+    left_of(marineros, (var(), var(), var(), 'Sloop'), (var(), 'Neal Bundt', var(), var())),
     #4. The owner of the Quick Silver was either the sailor in the sloop or the competitor who finished in 282 days.
     lany(membero((var(), var(), 'Quick silver', 'Sloop'), marineros), eq((282, var(), 'Quick silver', var()), marinero4)),
     neq((282, var(), 'Quick silver', 'Sloop'), marineros),
     #5. Gil Baxter took 4 fewer days to finish than Debra Decker.
     left_of(marineros, (var(), 'Gil Baxter', var(), var()), (var(), 'Debra Decker', var(), var())),
-    #6. Gil Baxter didn't sail the Bay Hawk.
-    neq((var(), 'Gil Baxter', 'Bay hawk', var()), marineros),
+
     #7. Vicky Estes took 8 more days to finish than the owner of the Dream Walker.
     right_of(marineros, (var(), 'Vicky Estes', var(), var()), (var(), var(), 'Dream walker', var()), 2),
     #8. Ed Alexander was either the sailor in the gaff cutter or the owner of the Dream Walker.
@@ -46,16 +46,31 @@ def carrera(marineros):
     #14. Ed Alexander took somewhat more days to finish than the owner of the Shady Lady.
     somewhat_right_of(marineros, (var(), 'Ed Alexander', var(), var()), (var(), var(), 'Shady lady', var())),
     #15. Datos no mencionados
-    membero((var(), 'Tara Carrol', var(), var()), marineros),
+    membero((var(), 'Tara Carroll', var(), var()), marineros),
     membero((var(), var(), 'Alpha one', var()), marineros),
+    membero((var(), var(), 'Bay hawk', var()), marineros),
+    membero((var(), var(), 'Sea Dart', var()), marineros),
     membero((var(), var(), var(), 'Catamaran'), marineros),
+    membero((var(), var(), var(), 'Pilot cutter'), marineros),
+    membero((var(), var(), var(), 'Trimaran'), marineros),
+
+    
+
+    #1. The owner of the Dream Walker didn't use the pilot cutter.
+    membero(marineros, ('Dream walker', 'Pilot cutter'), (2,3)),
+    #6. Gil Baxter didn't sail the Bay Hawk.
+    membero(marineros, ('Gil Baxter', 'Bay hawk'), (1,2)),
+#10. The competitor in the trimaran, the sailor who finished in 290 days, the competitor who finished in 274 days, the owner of the Sea Dart, the owner of the Dream Walker and Gil Baxter were all different people.
+    differents(marineros, ((290, 274,), ('Gil Baxter',), ('Sea Dart', 'Dream Walker',), ('Trimaran',)))
   )
+
+start = perf_counter()
 solutions = run(0, marineros, carrera(marineros),
-                    #10. The competitor in the trimaran, the sailor who finished in 290 days, the competitor who finished in 274 days, the owner of the Sea Dart, the owner of the Dream Walker and Gil Baxter were all different people.
-                    differents(marineros, ((274, 290,), ('Gil Baxter',), ('Sea dart', 'Dream walker',), ('Trimaran',))),
-                    #1. The owner of the Dream Walker didn't use the pilot cutter.
-                    differents(marineros, ((var(),), (var(),), ('Dream walker',), ('Pilot cutter',))),
-                    #2. The person who finished in 282 days sailed the Confluence.
-                    differents(marineros, ((282,), (var(),), ('Confluence',), (var(),)))
-                )
+)
+
+end = perf_counter()
+
 print(solutions)
+
+execution_time = (end - start)
+print(execution_time)
